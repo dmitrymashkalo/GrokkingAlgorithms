f032ac56a9c2133f7e56d920caee706cec75f85a
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-03-14 17:14:03 +0100

    Little changes

--- 02_Selection_sort/selection_sort.py
+++ 02_Selection_sort/selection_sort.py
@@ -1,13 +1,18 @@
 def selection_sort(arr: list) -> list:
-    n = len(arr)
 
-    for i in range(n):
+    length = len(arr)
+
+    for i in range(length):
         min_index = i
 
-        for j in range(i + 1, n):
+        for j in range(i + 1, length):
             if arr[j] < arr[min_index]:
                 min_index = j
 
         arr[i], arr[min_index] = arr[min_index], arr[i]
 
     return arr
+
+
+arr = [0, 1, 2, 3, 4, 5]
+print(len(arr))
